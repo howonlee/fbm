7f003d2dbe961d515aed47b02f64e8bb25c7d44c
Howon Lee <howon@howonlee.com>
2014-08-12 11:44:29 -0700

    some results with the fractional motion, but need more work

--- sim.py
+++ sim.py
@@ -2,6 +2,7 @@ import numpy as np
 import numpy.linalg as np_lin
 import numpy.random as np_rand
 import matplotlib.pyplot as plt
+from random import choice
 import cPickle, sys
 
 def is_pos_def(x):
@@ -19,14 +20,30 @@ def normalize(arr):
 def get_corpus(filename):
 	corpus = []
 	with open(filename, "r") as corpus_file:
-		for word in list(corpus_file.read()):
+		for word in list(corpus_file.read().split()):
 			corpus.append(word)
 	return corpus
 
+def get_bigrams(ls):
+	return zip(ls, ls[1:])
 
-#use cumsum and turn into markov chain eventually
+def generate_sentence(init, ls, corpus, max_itr):
+	words = ""
+	curr_idx = init
+	curr_itr = 0
+	while True:
+		curr_itr += 1
+		next_idx = choice(ls.get(curr_idx, [-1]))
+		words = words + " " + corpus[curr_idx-1]
+		if next_idx == -1:
+			break
+		elif curr_itr >= max_itr:
+			break
+		else:
+			curr_idx = next_idx
+	return words
+
 if __name__ == "__main__":
-	assert len(sys.argv) == 2
 	dim_len = 2000
 	times = range(1, dim_len + 1)
 	gamma = np.zeros((dim_len, dim_len))
@@ -41,7 +58,7 @@ if __name__ == "__main__":
 	vec = np_rand.normal(size=(dim_len,))
 	u = np.dot(sigma, vec)
 	u = normalize(u)
-	if sys.argv[1] == "plot":
+	if len(sys.argv) == 2 and sys.argv[1] == "plot":
 		plt.plot(range(dim_len), u, 'b-')
 		plt.show()
 	else:
@@ -52,7 +69,9 @@ if __name__ == "__main__":
 		corpus = get_corpus("corpus.txt")
 		len_corpus = len(corpus)
 		u = np.ceil(u * len_corpus).astype(int).tolist()
-		text = ""
-		for i in u:
-			text = text + corpus[i- 1]
-		print text
+		tups = {}
+		for i in get_bigrams(u):
+			if not tups.get(i[0], None):
+				tups[i[0]] = []
+			tups[i[0]].append(i[1])
+		print generate_sentence(0, tups, corpus, 5000)
